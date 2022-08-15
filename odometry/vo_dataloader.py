c7cbddf0b061f5fd81544d9c9d37b4380bf373fb
bogarrichard <bgyricsi@gmail.com>
2022-08-15 17:25:16 +0200

    Dataset and Dataloader for custom KITTI training

---
+++ odometry/vo_dataloader.py
@@ -0,0 +1,54 @@
+import torch
+import time
+from helpers import log
+
+class CustomKITTIDataLoader():
+    def __init__(self, dataset, batch_size) -> None:
+        self.batch_size = batch_size
+        self.dataset = dataset
+        self.indexes = torch.randperm(len(self.dataset))
+
+
+    def __iter__(self):
+        self.indexes_index = 0        
+        return self
+
+
+    def __next__(self):
+        if (self.indexes_index+self.batch_size) >= len(self.indexes):
+            raise StopIteration
+
+        images, fl, true_rot, true_tr = [], [], [], []
+
+        #actual_time = time.time()
+
+        for j in range(self.batch_size):
+            imgs, flow, true_rotation, true_translation = self.dataset[int(self.indexes[self.indexes_index+j])]
+            #print("Dataset lookup: ", (time.time()-actual_time)*1000)
+            #actual_time = time.time()
+
+            images.append(imgs)
+            fl.append(flow)
+            true_rot.append(true_rotation)
+            true_tr.append(true_translation)
+
+            #print("Append: ", (time.time()-actual_time)*1000)
+            #actual_time = time.time()
+
+        #actual_time = time.time()
+        #print()
+
+        images = torch.stack(images, dim=0)
+        fl = torch.stack(fl, dim=0)
+        true_rot = torch.stack(true_rot, dim=0)
+        true_tr = torch.stack(true_tr, dim=0)
+
+        #print("Stack: ", (time.time()-actual_time)*1000)
+        #actual_time = time.time()
+
+        self.indexes_index = self.indexes_index+self.batch_size
+        return int(self.indexes_index/self.batch_size), (images, fl, true_rot, true_tr)
+
+
+    def __len__(self):
+        return int(len(self.dataset)/self.batch_size)
--- odometry/vo_datasets.py
+++ odometry/vo_datasets.py
@@ -1,77 +1,22 @@
 from os import path
 import glob
 import numpy as np
-from PIL import Image
 import torch
 from torch.utils import data
 from scipy.spatial.transform import Rotation as R
 from GMA.core.utils.utils import InputPadder
 from torchvision.transforms import Resize
+import torchvision.io as io
 
-from helpers import log, matrix2euler, euler2matrix
+from helpers import log, matrix2euler
 
-
-class KittiOdometryDataset(data.Dataset):
-    def __init__(self, data_path, sequence, precomputed_flow=False, sequence_length=4, device='cuda'):
-        super(KittiOdometryDataset, self).__init__()
-        
-        self.sequence = sequence
-        self.precomputed_flow = precomputed_flow
-        self.N = sequence_length+1
-        self.device = device
-        self.resize = Resize((376, 1248))
-
-        self.data_path = path.join(data_path, 'dataset')
-
-        self.im_path = path.join(self.data_path, "sequences", sequence, "image_2")
-        self.flow_path = path.join(self.data_path, "flows", sequence)
-        self.poses = np.loadtxt(path.join(self.data_path, 'poses', self.sequence+'.txt'), dtype=np.double)
-
-        im_file = "000000.png"
-        img = self.load_image(path.join(self.im_path, im_file))
-        self.padder = InputPadder(img.shape)
-        self.len = len(glob.glob(self.flow_path+"/*.pt"))
-
-    def __len__(self):
-        #return self.poses.shape[0]-self.N
-        len = self.len-self.N
-        return len
-
-    def __getitem__(self, index):
-            
-            # Getting pose difference as rotation and translation vectors
-            poses_n = [self.poses[index+i, :] for i in range(0, self.N)]
-            delta_transforms = [self.preprocess_poses_euler(poses_n[i], poses_n[i+1]) for i in range(0, (self.N-1))]
-
-            # Getting the image file names
-            img_files = ['0'*(6-len(str(index+i))) + str(index+i) + ".png" for i in range(0, self.N)]
-
-            imgs = torch.stack([self.padder.pad(self.load_image(path.join(self.im_path, im_file)))[0] for im_file in img_files], dim=0)
-            imgs1 = torch.stack([imgs[i] for i in range(0, (self.N-1))], dim=0)
-            imgs2 = torch.stack([imgs[i] for i in range(1, self.N)], dim=0)
-
-            delta_rotations = [delta_transforms[i][0] for i in range(len(delta_transforms))]
-            delta_translations = [delta_transforms[i][1] for i in range(len(delta_transforms))]
-            delta_rotations = torch.stack(delta_rotations)
-            delta_translations = torch.stack(delta_translations)
+import time
 
 
-            if not self.precomputed_flow:
-                return imgs1, imgs2, delta_rotations, delta_translations
-            else:
-                flow_files = ['0'*(6-len(str(index+i))) + str(index+i) + ".pt" for i in range(0, self.N-1)]
-                flows = [torch.load(path.join(self.flow_path, flow_file)) for flow_file in flow_files]
-                flows = torch.stack(flows, dim=0)
-                
-                return imgs1, imgs2, flows, delta_rotations, delta_translations
-
-
-
-    def load_image(self, imfile):
-        img = np.expand_dims(np.array(Image.open(imfile)).astype(np.uint8), -1)
-        img = torch.from_numpy(img).squeeze().permute(2, 0, 1).float()
-        return img[None]
-
+class OdometryDataset(data.Dataset):
+    def __init__(self):
+            super(OdometryDataset, self).__init__()
+    
 
     def preprocess_poses_euler(self,  pose1, pose2):
         # Stacking the matrix rows stored in the lines of the array
@@ -94,29 +39,148 @@ class KittiOdometryDataset(data.Dataset):
         
 
 
-    def prepocess_poses_euler2(self,  pose1, pose2):
-        # Stacking the matrix rows stored in the lines of the array
-        pose1 = np.array([pose1[0:4], pose1[4:8], pose1[8:12]])
-        pose2 = np.array([pose2[0:4], pose2[4:8], pose2[8:12]])
+class KittiOdometryDataset(OdometryDataset):
+    def __init__(self, data_path, sequence, precomputed_flow=False, sequence_length=4, device='cuda'):
+        super(KittiOdometryDataset, self).__init__()
         
-        # Getting rotation matrix
-        abs_rotation1 = pose1[:, :3]
-        abs_rotation2 = pose2[:, :3]
-        
-        # Getting position vectors
-        translation1 = pose1[:, -1]
-        translation2 = pose2[:, -1]
-        # Converting rotation matrix to euler angle vector
-        abs_rotation1 = torch.from_numpy(abs_rotation1)
-        abs_rotation2 = torch.from_numpy(abs_rotation2)
-        # Calculating differences         
-        #delta_rotation = matrix2euler(torch.matmul(abs_rotation1, abs_rotation2))
-        delta_rotation = matrix2euler(torch.matmul(abs_rotation2, torch.transpose(abs_rotation1, 0, 1)))
-        
-        delta_translation = torch.from_numpy(np.array(translation2 - translation1))
-        #delta_translation = torch.from_numpy(abs_rotation1.inv().apply(delta_translation))
-        delta_translation = torch.matmul(torch.transpose(abs_rotation1, 0, 1), delta_translation)
-        return [delta_rotation, delta_translation]
+        self.sequence = sequence
+        self.precomputed_flow = precomputed_flow
+        self.N = sequence_length
+        self.device = device
+        self.resize = Resize((376, 1248))
 
-    
-    
+        self.data_path = path.join(data_path, 'dataset')
+
+        self.im_path = path.join(self.data_path, "sequences", sequence, "image_2")
+        self.flow_path = path.join(self.data_path, "flows", sequence)
+        self.poses = np.loadtxt(path.join(self.data_path, 'poses', self.sequence+'.txt'), dtype=np.double)
+
+        im_file = "000000.png"
+        img = io.read_image(path.join(self.im_path, im_file)).float()
+        self.padder = InputPadder(img.shape)
+        self.len = len(glob.glob(self.im_path+"/*.png"))-self.N
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+            
+            # Getting pose difference as rotation and translation vectors
+            poses_n = [self.poses[index+i, :] for i in range(0, self.N+1)]
+            delta_transforms = [super(KittiOdometryDataset, self).preprocess_poses_euler(poses_n[i], poses_n[i+1]) for i in range(0, (self.N))]
+
+            # Getting the image file names
+            img_files = ['0'*(6-len(str(index+i))) + str(index+i) + ".png" for i in range(0, self.N+1)]
+
+            imgs = torch.stack([self.padder.pad(io.read_image(path.join(self.im_path, im_file)).float())[0] for im_file in img_files], dim=0)
+            #imgs1 = torch.stack([imgs[i] for i in range(0, (self.N-1))], dim=0)
+            #imgs2 = torch.stack([imgs[i] for i in range(1, self.N)], dim=0)
+            imgs = torch.stack([imgs[i] for i in range(0, self.N+1)], dim=0)
+
+            delta_rotations = [delta_transforms[i][0] for i in range(len(delta_transforms))]
+            delta_translations = [delta_transforms[i][1] for i in range(len(delta_transforms))]
+            delta_rotations = torch.stack(delta_rotations)
+            delta_translations = torch.stack(delta_translations)
+
+
+            if not self.precomputed_flow:
+                #return imgs1, imgs2, delta_rotations, delta_translations
+                return imgs, delta_rotations, delta_translations
+            else:
+                flow_files = ['0'*(6-len(str(index+i))) + str(index+i) + ".pt" for i in range(0, self.N)]
+                flows = [torch.load(path.join(self.flow_path, flow_file)) for flow_file in flow_files]
+                flows = torch.stack(flows, dim=0)
+                
+                return imgs, flows, delta_rotations, delta_translations
+
+
+class CustomKittiOdometryDataset(OdometryDataset):
+    def __init__(self, data_path, sequences=['00'], precomputed_flow=False, sequence_length=4, device='cuda'):
+        super(CustomKittiOdometryDataset, self).__init__()
+
+        self.sequences = sequences
+        self.precomputed_flow = precomputed_flow
+        self.N = sequence_length
+        self.device = device
+
+        self.data_path = path.join(data_path, 'dataset')
+        self.sequence_lengths = [0]
+        self.sequence_poses = []
+
+        # Checking type of sequences
+        assert type(sequences) is list, "Sequences should be given as a list of strings!"
+        for sequence in sequences:
+            # Checking type and existence of each sequence
+            assert type(sequence) is str, "Sequence should be given as a string!"
+            if not path.isfile(path.join(self.data_path, "poses", sequence+".txt")):
+                raise Exception("Sequence " + sequence + " doesn't exist!")
+            
+            im_path = path.join(self.data_path, "sequences", sequence, "image_2")
+            sequence_length = len(glob.glob(im_path+"/*.png"))-self.N
+            self.sequence_lengths.append(self.sequence_lengths[-1]+sequence_length) 
+            
+            self.sequence_poses.append(np.loadtxt(path.join(self.data_path, 'poses', sequence+'.txt'), dtype=np.double))
+
+        self.sequence_lengths = self.sequence_lengths[1:]
+        assert len(sequences) == len(self.sequence_lengths), "Sequence lengths are not as many as sequences"
+
+        img = io.read_image(path.join(self.data_path, "sequences", '00', "image_2", "000000.png")).float()
+        self.resize = Resize((img.shape[-2], img.shape[-1]))
+        self.padder = InputPadder(img.shape)
+
+
+    def __len__(self):
+        return self.sequence_lengths[-1]
+
+
+    def __getitem__(self, index):
+            #actual_time = time.time()
+
+            sequence_index = 0
+            index_offset = 0
+            # Finding the sequence
+            for i in range(len(self.sequence_lengths)):
+                if index >= self.sequence_lengths[i]:
+                    sequence_index = sequence_index+1
+                    index_offset = self.sequence_lengths[i]
+
+            index = index-index_offset
+
+            #log("Sequence_search: ", time.time()-actual_time)
+            #actual_time = time.time()
+
+            # Getting pose difference as rotation and translation vectors
+            poses_n = [self.sequence_poses[sequence_index][index+i, :] for i in range(0, self.N+1)]
+            delta_transforms = [super(CustomKittiOdometryDataset, self).preprocess_poses_euler(poses_n[i], poses_n[i+1]) for i in range(0, (self.N))]
+            delta_rotations = [delta_transforms[i][0] for i in range(len(delta_transforms))]
+            delta_translations = [delta_transforms[i][1] for i in range(len(delta_transforms))]
+            delta_rotations = torch.stack(delta_rotations)
+            delta_translations = torch.stack(delta_translations)
+            
+            #log("Transforms: ", time.time()-actual_time)
+            #actual_time = time.time()
+
+            # Generating image file names from index
+            im_path = path.join(self.data_path, "sequences", self.sequences[sequence_index], "image_2")
+            img_files = ['0'*(6-len(str(index+i))) + str(index+i) + ".png" for i in range(0, self.N+1)]
+            imgs = torch.stack([self.padder.pad(io.read_image(path.join(im_path, im_file)).float())[0] for im_file in img_files], dim=0)
+            imgs = torch.stack([imgs[i] for i in range(0, self.N+1)], dim=0)
+            imgs = self.resize(imgs)
+
+            #log("Image load: ", time.time()-actual_time)
+            #actual_time = time.time()
+
+            if not self.precomputed_flow:
+                return imgs, delta_rotations, delta_translations
+            else:
+                flow_path = path.join(self.data_path, "flows", self.sequences[sequence_index])
+                # Generating flow file names from index
+                flow_files = ['0'*(6-len(str(index+i))) + str(index+i) + ".pt" for i in range(0, self.N)]
+                flows = [torch.load(path.join(flow_path, flow_file)) for flow_file in flow_files]
+                flows = torch.stack(flows, dim=0).squeeze()
+                flows = self.resize(flows)
+
+                #log("Flow fload: ", time.time()-actual_time)
+                #actual_time = time.time()
+                
+                return imgs, flows, delta_rotations, delta_translations
