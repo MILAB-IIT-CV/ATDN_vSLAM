3f39b6aef5a60a046430b7473913e91479ceeb9a
bogarrichard <bgyricsi@gmail.com>
2022-08-23 21:33:05 +0200

    Sum of com losses

--- odometry/vo_loss.py
+++ odometry/vo_loss.py
@@ -4,20 +4,21 @@ from helpers import euler2matrix, matrix2euler
 
 class CLVO_Loss():
 
-    def __init__(self, alpha=1):
+    def __init__(self, alpha=1, w=3):
         self.last_com = 0
         self.stage = 0
         self.alpha = alpha
         self.delta = 1
         self.khi = 100
+        self.w = w
 
 
-    def __call__(self, pred_transforms, true_transforms, w=4, device='cuda'):
+    def __call__(self, pred_transforms, true_transforms, device='cuda'):
 
         # --------------------------------------
         # Dimension checking and data extraction
         # --------------------------------------
-
+        w = self.w
         assert len(pred_transforms) == len(true_transforms), "Loss: Length of pred and true are not equal"
 
         pred_rot, pred_tr = pred_transforms
@@ -30,18 +31,20 @@ class CLVO_Loss():
         L_rel = 0
         #for i in range(len(pred_rot)):
         #L_rel = L_rel + self.transform_loss(pred_rot[i], pred_tr[i], true_rot[i], true_tr[i], device)
-        L_rel = torch.cumsum(self.transform_loss(pred_rot, pred_tr, true_rot, true_tr, device), -2)[:, -1, -1].mean()
-        #log("Rel loss: ", L_rel.shape)
+        L_rel = self.transform_loss(pred_rot, pred_tr, true_rot, true_tr, device).sum(-1)
+        #log("Rel loss1: ", L_rel.shape)
+        #L_rel = L_rel.mean()
 
         # -------------------
         # Composite pose loss
         # -------------------
-
+        log("W", w)
         L_com = []
         for i in range(len(pred_rot)):
             L_com.append(self.com_loss([pred_rot[i], pred_tr[i]], [true_rot[i], true_tr[i]], w=w, device=device))
-        #log("Comm, loss", L_com)
-        L_com = torch.stack(L_com, dim=0)[:, -1].mean()
+        L_com = torch.stack(L_com, dim=0).sum(-1)
+        log("Com loss", L_com.shape)
+        #[:, -1].mean()
         #log("Com loss: ", L_com.shape)
         #L_com = L_com if L_com > self.last_com else 0
         
@@ -59,7 +62,7 @@ class CLVO_Loss():
         return L_total.mean()
 
 
-    def com_loss(self, pred_transforms, true_transforms, w=2, device='cuda'):
+    def com_loss(self, pred_transforms, true_transforms, w, device='cuda'):
 
         # Separating rotations and translations
         pred_rot, pred_tr = pred_transforms
@@ -68,7 +71,7 @@ class CLVO_Loss():
         # Converting euler vector and translation vector to homogenous transformation matrix
         pred_homogenous_array = []
         true_homogenous_array = []
-        for i in range(w):
+        for i in range(len(pred_rot)):
             # Converting predicted
             # Euler to matrix rotation representation
             rot_mat = euler2matrix(pred_rot[i], device=device)
@@ -90,25 +93,28 @@ class CLVO_Loss():
             # Appending matrix to array
             true_homogenous_array.append(mat)
 
+        losses = []
+        for j in range(len(pred_homogenous_array)-w):
+            # Creating the combining the transformations to a 
+            pred_comm = pred_homogenous_array[0]
+            true_comm = true_homogenous_array[0]
+            for i in range(j+1, j+w+1):
+                pred_comm = torch.matmul(pred_comm, pred_homogenous_array[i])
+                true_comm = torch.matmul(true_comm, true_homogenous_array[i])
+            # Converting back to euler and separaing the matrix
+            pred_comm_rot = matrix2euler(pred_comm[:3, :3])
+            pred_comm_tr = pred_comm[:3, -1]
 
-        # Creating the combining the transformations to a 
-        pred_comm = pred_homogenous_array[0]
-        true_comm = true_homogenous_array[0]
-        for i in range(1, w):
-            pred_comm = torch.matmul(pred_comm, pred_homogenous_array[i])
-            true_comm = torch.matmul(true_comm, true_homogenous_array[i])
+            # Converting back to euler and separaing the matrix
+            true_comm_rot = matrix2euler(true_comm[:3, :3])
+            true_comm_tr = true_comm[:3, -1]
+            
+            loss = self.transform_loss(pred_comm_rot, pred_comm_tr, true_comm_rot, true_comm_tr, device=device)
+            losses.append(loss)
 
-        # Converting back to euler and separaing the matrix
-        pred_comm_rot = matrix2euler(pred_comm[:3, :3])
-        pred_comm_tr = pred_comm[:3, -1]
-
-        # Converting back to euler and separaing the matrix
-        true_comm_rot = matrix2euler(true_comm[:3, :3])
-        true_comm_tr = true_comm[:3, -1]
-        
-        loss = self.transform_loss(pred_comm_rot, pred_comm_tr, true_comm_rot, true_comm_tr, device=device)
-
+        loss = torch.stack(losses, dim=0)
         return loss
+
 
     def transform_loss(self, pred_rotation, pred_translation, true_rotation, true_translation, device='cuda'):
 
@@ -123,8 +129,8 @@ class CLVO_Loss():
         diff_rotation = pred_rotation-true_rotation
         
         # Mean is changed to be calculated in call method
-        norm_rotation = torch.cumsum((diff_rotation**2), -1)
-        norm_translation = torch.cumsum((diff_translation**2), -1)
+        norm_rotation = (diff_rotation**2).sum(-1)
+        norm_translation = (diff_translation**2).sum( -1)
 
         loss = self.delta*norm_translation + self.khi*norm_rotation
         return loss
