78b0bf072805f6ab8a763021f3e7d3f345879ab0
bogarrichard <bgyricsi@gmail.com>
2022-08-23 21:57:59 +0200

    Separating arguments and gma parameters

---
+++ arguments.py
@@ -0,0 +1,29 @@
+import yaml
+
+
+class Arguments():
+    def __init__(self):
+        # Data parameters
+        self.data_path : str
+        self.keyframes_path : str
+
+        # Training parameters
+        self.device : str
+        self.epochs : int
+        self.lr : float
+        self.wd : float
+        self.epsilon : float
+        self.batch_size : int
+        self.sequence_length : int
+        self.weight_file : str
+        self.log_file : str
+        self.weight_decay : bool
+        self.train_sequences : list
+        self.precomputed_flow: bool
+        self.stage : int
+        self.w : int
+
+    @classmethod
+    def get_arguments(cls, config_path="config.yaml"):
+        args = yaml.load(open(config_path, "r"), yaml.Loader)
+        return args
---
+++ gma_parameters.py
@@ -0,0 +1,34 @@
+class GMA_Parameters():
+    def __init__(self):
+
+        # GMA parameters
+        self.model = "GMA/checkpoints/gma-kitti.pth"
+        self.dataset = "kitti"
+        self.iters = 12
+        self.num_heads = 1
+        self.position_only = False
+        self.position_and_content = False
+        self.mixed_precision = True
+        self.replace = False
+        self.no_alpha = False
+        self.no_residuals = False
+        self.model_name = self.model
+        self.path = "imgs"       
+        
+        self.dictionary = {
+                           "model" : self.model,
+                         "dataset" : self.dataset, 
+                           "iters" : self.iters, 
+                       "num_heads" : self.num_heads, 
+                   "position_only" : self.position_only, 
+            "position_and_content" : self.position_and_content, 
+                 "mixed_precision" : self.mixed_precision,
+                         "replace" : self.replace,
+                        "no_alpha" : self.no_alpha,
+                    "no_residuals" : self.no_residuals,
+                            "imgs" : self.path,
+                      "model_name" : self.model
+        }
+
+    def __contains__(self, key):
+        return key in self.dictionary
