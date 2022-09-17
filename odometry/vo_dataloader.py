945d1440119a115998dff0089b1eb0c1ef7b43fb
bogarrichard <bgyricsi@gmail.com>
2022-09-17 10:41:55 +0200

    Flow only input: dataset, dataloader and training

--- odometry/vo_dataloader.py
+++ odometry/vo_dataloader.py
@@ -52,3 +52,48 @@ class CustomKITTIDataLoader():
 
     def __len__(self):
         return int(len(self.dataset)/self.batch_size)
+
+
+class FlowKITTIDataLoader():
+    def __init__(self, dataset, batch_size) -> None:
+        self.batch_size = batch_size
+        self.dataset = dataset
+        self.indexes = torch.randperm(len(self.dataset))
+
+
+    def __iter__(self):
+        self.indexes_index = 0        
+        return self
+
+
+    def __next__(self):
+        if (self.indexes_index+self.batch_size) >= len(self.indexes):
+            raise StopIteration
+
+        fl, true_rot, true_tr = [], [], []
+
+        #actual_time = time.time()
+
+        for j in range(self.batch_size):
+            flow, true_rotation, true_translation = self.dataset[int(self.indexes[self.indexes_index+j])]
+
+            fl.append(flow)
+            true_rot.append(true_rotation)
+            true_tr.append(true_translation)
+
+        #actual_time = time.time()
+        #print()
+
+        fl = torch.stack(fl, dim=0)
+        true_rot = torch.stack(true_rot, dim=0)
+        true_tr = torch.stack(true_tr, dim=0)
+
+        #print("Stack: ", (time.time()-actual_time)*1000)
+        #actual_time = time.time()
+
+        self.indexes_index = self.indexes_index+self.batch_size
+        return int(self.indexes_index/self.batch_size), (fl, true_rot, true_tr)
+
+
+    def __len__(self):
+        return int(len(self.dataset)/self.batch_size)
--- odometry/vo_datasets.py
+++ odometry/vo_datasets.py
@@ -178,3 +178,83 @@ class CustomKittiOdometryDataset(OdometryDataset):
                 #actual_time = time.time()
                 
                 return imgs, flows, delta_rotations, delta_translations
+
+
+class FlowKittiDataset(OdometryDataset):
+    def __init__(self, data_path, sequences=['00'], precomputed_flow=False, sequence_length=4, device='cuda'):
+        super(FlowKittiDataset, self).__init__()
+
+        self.sequences = sequences
+        self.precomputed_flow = precomputed_flow
+        self.N = sequence_length
+        self.device = device
+
+        self.data_path = path.join(data_path, 'dataset')
+        self.sequence_lengths = [0]
+        self.sequence_poses = []
+
+        # Checking type of sequences
+        assert type(sequences) is list, "Sequences should be given as a list of strings!"
+        for sequence in sequences:
+            # Checking type and existence of each sequence
+            assert type(sequence) is str, "Sequence should be given as a string!"
+            if not path.isfile(path.join(self.data_path, "poses", sequence+".txt")):
+                raise Exception("Sequence " + sequence + " doesn't exist!")
+            
+            im_path = path.join(self.data_path, "sequences", sequence, "image_2")
+            sequence_length = len(glob.glob(im_path+"/*.png"))-self.N
+            self.sequence_lengths.append(self.sequence_lengths[-1]+sequence_length) 
+            
+            pose = np.loadtxt(path.join(self.data_path, 'poses', sequence+'.txt'), dtype=np.double)
+            self.sequence_poses.append(torch.from_numpy(pose))
+
+        self.sequence_lengths = self.sequence_lengths[1:]
+        assert len(sequences) == len(self.sequence_lengths), "Sequence lengths are not as many as sequences"
+
+        img = io.read_image(path.join(self.data_path, "sequences", '00', "image_2", "000000.png")).float()
+        self.resize = Resize((img.shape[-2], img.shape[-1]))
+        self.padder = InputPadder(img.shape)
+
+
+    def __len__(self):
+        return self.sequence_lengths[-1]
+
+
+    def __getitem__(self, index):
+            #actual_time = time.time()
+
+            sequence_index = 0
+            index_offset = 0
+            # Finding the sequence
+            for i in range(len(self.sequence_lengths)):
+                if index >= self.sequence_lengths[i]:
+                    sequence_index = sequence_index+1
+                    index_offset = self.sequence_lengths[i]
+
+            index = index-index_offset
+
+            #log("Sequence_search: ", time.time()-actual_time)
+            #actual_time = time.time()
+
+            # Getting pose difference as rotation and translation vectors
+            poses_n = [self.sequence_poses[sequence_index][index+i, :] for i in range(0, self.N+1)]
+            delta_transforms = [super(FlowKittiDataset, self).preprocess_poses_euler(poses_n[i], poses_n[i+1]) for i in range(0, (self.N))]
+            delta_rotations = [delta_transforms[i][0] for i in range(len(delta_transforms))]
+            delta_translations = [delta_transforms[i][1] for i in range(len(delta_transforms))]
+            delta_rotations = torch.stack(delta_rotations)
+            delta_translations = torch.stack(delta_translations)
+
+            #log("Image load: ", time.time()-actual_time)
+            #actual_time = time.time()
+
+            flow_path = path.join(self.data_path, "flows", self.sequences[sequence_index])
+            # Generating flow file names from index
+            flow_files = ['0'*(6-len(str(index+i))) + str(index+i) + ".pt" for i in range(0, self.N)]
+            flows = [torch.load(path.join(flow_path, flow_file)) for flow_file in flow_files]
+            flows = torch.stack(flows, dim=0).squeeze()
+            flows = self.resize(flows)
+
+            #log("Flow fload: ", time.time()-actual_time)
+            #actual_time = time.time()
+                
+            return flows, delta_rotations, delta_translations
