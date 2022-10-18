24f678b18a6b98e295cf2a669e0aa8f705a0edf7
bogarrichard <bgyricsi@gmail.com>
2022-10-18 16:20:14 +0200

    helpers for depth prediction

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentError
 import sys, os
 sys.path.insert(0, os.path.abspath(".."))
+import numpy as np
 import torch
 
 
@@ -61,6 +62,37 @@ def matrix2line(matrix):
     pose = torch.cat([matrix[0], matrix[1], matrix[2]], dim=0)
 
     return pose
+
+
+def read_calib(path, include_rect=False):
+    calib = np.loadtxt(path, dtype=str)
+    calib = calib[1][1:].astype(np.float32)
+    calib = torch.from_numpy(calib).view((3, 4))
+    if include_rect:
+        calib = torch.cat([calib, torch.tensor([[0, 0, 0, 1]])])
+    else:
+        calib = calib[:, :-1]
+
+    return calib
+
+
+def project_depth(depth, calib):
+    height, width = depth.shape[-2], depth.shape[-1]
+    
+    tensor_u = torch.arange(0, width, 1).repeat((height, 1))
+    tensor_v = torch.arange(0, height, 1).repeat((width, 1)).permute(1, 0)
+
+    h_u = tensor_u*depth
+    h_v = tensor_v*depth
+
+    homogenous = torch.stack([h_u, h_v, depth], dim=-1)
+    
+    flattened_h = homogenous.view((height*width, 3)).permute(1, 0)
+    calibi = torch.inverse(calib)
+    projected = torch.matmul(calibi, flattened_h).permute(1, 0)
+    projected = projected.view((height, width, 3)).permute(2, 0, 1)
+    
+    return projected
 
 
 def convert_to_KITTI_format(rotations_array, translations_array):
