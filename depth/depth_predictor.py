80407964b7aa90b7436bf6c048bfa543ead88d1f
bogarrichard <bgyricsi@gmail.com>
2022-10-19 22:04:07 +0200

    Lightweight depth predictor

--- depth/depth_predictor.py
+++ depth/depth_predictor.py
@@ -8,22 +8,17 @@ class DepthPredictor(nn.Module):
         super().__init__()
         
         self.net = nn.Sequential(
-            conv.Conv(in_channels=8, out_channels=8, kernel_size=7, padding=3),
-            conv.ResidualConv(in_channels=8, out_channels=16),
-            conv.ResidualConv(in_channels=16, out_channels=32),
-            conv.ResidualConv(in_channels=32, out_channels=64),
-            conv.ResidualConv(in_channels=64, out_channels=64),
-            conv.ResidualConv(in_channels=64, out_channels=64),
-            conv.ResidualConv(in_channels=64, out_channels=64),
-            conv.ResidualConv(in_channels=64, out_channels=32),
-            conv.ResidualConv(in_channels=32, out_channels=16),
-            conv.ResidualConv(in_channels=16, out_channels=8),
-            conv.ResidualConv(in_channels=8, out_channels=4),
-            nn.Conv2d(in_channels=4, out_channels=2, kernel_size=3, padding=1)
+            conv.Conv(in_channels=8, out_channels=16, kernel_size=7, padding=3),
+            conv.Conv(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            conv.Conv(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            conv.Conv(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            conv.Conv(in_channels=64, out_channels=32, kernel_size=3, padding=1),
+            conv.Conv(in_channels=32, out_channels=16, kernel_size=3, padding=1),
+            conv.Conv(in_channels=16, out_channels=8, kernel_size=3, padding=1),
+            nn.Conv2d(in_channels=8, out_channels=2, kernel_size=3, padding=1)
         )
 
 
-    def forward(self, flow, rgb1, rgb2):
-        input = torch.cat([flow, rgb1, rgb2], dim=1)
+    def forward(self, input):
         depths = self.net(input)
         return depths[:, 0, :, :], depths[:, 1, :, :]
