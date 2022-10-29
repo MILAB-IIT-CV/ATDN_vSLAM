Normalization fix

odometry/clvo.py
@@ -1,18 +1,19 @@
 import torch
 from torch import nn
+from torchvision.transforms import Normalize
 from general_layers.conv import Conv, ResidualConv
-from general_layers.vit import ViT
 
 from utils.helpers import log
 
 
 class CLVO(nn.Module):
-    """
+    __doc__="""
     Odometry network of the SLAM system.
-
-    * :param batch_size: Number of data batched as input and output
-    * :param in_channels: Number of channels of input data. Used for easy training experiments.
-    * :param device: Destination device for the model.
+    
+    :param batch_size: Number of data batched as input and output
+    :param in_channels: Number of channels of input data. Used for easy training experiments.
+    :param device: Destination device for the model.
+    
     """
     def __init__(
         self, 
@@ -32,6 +33,11 @@ class CLVO(nn.Module):
 
         self.activation = nn.PReLU
         self.MLP_activation = nn.PReLU
+
+        # ----------------------
+        # Implicit normalization
+        # ----------------------
+        self.normalize_flow = Normalize(mean=[ 1.4125, -0.9003], std=[41.2430, 41.1322])
 
         # ----------------------------------------------------
         # Feature extractor encoder module for the LSTM module
@@ -57,9 +63,7 @@ class CLVO(nn.Module):
             nn.Linear(in_features=1920, out_features=1024, bias=True),
             nn.Dropout(p=0.2),
             activation(inplace=True)
-        )
-
-        #self.vit = ViT(in_channels=16, device="cuda")
+        )        
 
         # --------------------------------------------------
         # Blocks of the LSTM (Long Short Term Memory) module
@@ -91,17 +95,23 @@ class CLVO(nn.Module):
                                                 bias=False)
 
 
-    def forward(self, *args):
-        flows = args[0]
-        #log("Flows: ", flows.shape)
+    def forward(self, flows : torch.Tensor):
+        """
+        The "call" function of the odometry estimator.
+
+        :param flows: Input optical flow values
+        :type flows: torch.Tensor
+        :return: The estimated euler angle rotation vector and estimated translation vector
+        :rtype: torch.Tensor
+        """
+
+        normalized_flows = self.normalize_flow(flows)
 
         # ------------------
         # Extracted features
         # ------------------
-        features = self.encoder_CNN(flows)
+        features = self.encoder_CNN(normalized_flows)
         #log("Encoder out: ", features.shape)
-        #vit_features = self.vit(features)[:, 0, :]
-        #log("VIT encoder out: ", vit_features.shape)
 
         # ----------------------
         # Long Short Term Memory
@@ -109,13 +119,11 @@ class CLVO(nn.Module):
         self.lstm_states1 = self.lstm1(features, self.lstm_states1)
         self.lstm_states2 = self.lstm2(self.lstm_states1[0], self.lstm_states2)
         lstm_out = self.lstm_states2[0]
-        #lstm_out = lstm_out.squeeze()
         #log("LSTM out:", lstm_out.shape)
 
         # ----------------------------------
         # Odometry module translation branch
         # ----------------------------------
-        #log("MLP input shape", mlp_input.shape)
         translations = self.translation_regressor(lstm_out)
         rotations = self.rotation_regressor(lstm_out)
 
@@ -133,13 +141,19 @@ class CLVO(nn.Module):
 
 
 class Regressor_MLP(nn.Module):
+    """
+    Regressor Multilayer Perceptron head of the odometry module.
+    
+    :param in_features: Input feature size
+    :param out_features: Output feature size
+    :param activation: The type of activation function used in the regressor
+    """
     def __init__(
         self, 
-        in_features, 
-        out_features, 
+        in_features : int, 
+        out_features : int, 
         activation = nn.PReLU, 
-        bias = True,
-        init = False
+        bias : bool = True
     ) -> None:
         super(Regressor_MLP, self).__init__()
 
@@ -155,4 +169,12 @@ class Regressor_MLP(nn.Module):
 
 
     def forward(self, x):
+        """
+        The "call" function of the regressor.
+
+        :param x: The input from the LSTM modules
+        :type x: torch.Tensor
+        :return: The estimated rotation or translation
+        :rtype: torch.Tensor
+        """
         return self.regressor(x)

train_ododmetry.py
@@ -10,7 +10,6 @@ import torch.utils.tensorboard
 # Project module imports
 from odometry.vo_loss import CLVO_Loss
 from odometry.vo_datasets import FlowKittiDataset
-from utils.normalization import FlowStandardization
 from odometry.clvo import CLVO
 from utils.helpers import log
 from utils.arguments import Arguments
@@ -19,7 +18,7 @@ import math
 
 
 
-def train(args, normalization, model, dataloader, odometry_loss, optimizer, scheduler, writer, epoch, log_vals=[]):
+def train(args, model, dataloader, odometry_loss, optimizer, scheduler, writer, epoch, log_vals=[]):
 
     for batch, (fl, true_rot, true_tr) in enumerate(dataloader):
 
@@ -31,7 +30,6 @@ def train(args, normalization, model, dataloader, odometry_loss, optimizer, sche
             flows = fl[:, j]
 
             flows = flows.squeeze().to(args.device)
-            flows = normalization(flows)
             input_data = flows
 
             pred_rotations, pred_translations = model(input_data)
@@ -72,9 +70,7 @@ def main():
     log("Flow augmentation: ", args.augment_flow)
     # Instantiating dataset and dataloader
     dataset = FlowKittiDataset(args.data_path, sequences=args.train_sequences, augment=args.augment_flow, sequence_length=args.sequence_length)
-    dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=True, num_workers=2, pin_memory=True, drop_last=True)
-
-    normalization =  FlowStandardization().eval()
+    dataloader = DataLoader(dataset=dataset, batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True, drop_last=True)
 
     model = CLVO(args.batch_size, in_channels=2).to(args.device)
     trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -107,7 +103,6 @@ def main():
         log_vals_actual = []
         
         train(args,
-              normalization,
               model, 
               dataloader, 
               loss, 