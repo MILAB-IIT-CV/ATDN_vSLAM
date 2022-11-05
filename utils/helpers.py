49ebbbaf930c9c2ec7f1753a9778b0c02b3dc59a
bogarrichard <bgyricsi@gmail.com>
2022-11-05 11:59:37 +0100

    Separating utilities to more submodules

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,3 @@
-from argparse import ArgumentError
 import sys, os
 sys.path.insert(0, os.path.abspath(".."))
 import numpy as np
@@ -6,6 +5,9 @@ import torch
 
 
 class ShapeLogLayer(torch.nn.Module):
+    """
+    Torch module for seamlessly logging output shapes in a torch.nn.Sequential model
+    """
     def __init__(self, message='') -> None:
         super(ShapeLogLayer, self).__init__()
         self.message = message
@@ -48,133 +50,4 @@ def log(*messages):
     print(msg_len*'-')
     print(message)
     print(msg_len*'-')
-
-
-def line2matrix(pose):
-    matrix = torch.stack([pose[0:4], pose[4:8], pose[8:12]], dim=0)
-    matrix = torch.cat([matrix, torch.tensor([[0, 0, 0, 1]])], dim=0)
-
-    return matrix
-
-
-def matrix2line(matrix):
-    # TODO: Test function
-    pose = torch.cat([matrix[0], matrix[1], matrix[2]], dim=0)
-
-    return pose
-
-
-def read_calib(path, include_rect=False):
-    calib = np.loadtxt(path, dtype=str)
-    calib = calib[1][1:].astype(np.float32)
-    calib = torch.from_numpy(calib).view((3, 4))
-    if include_rect:
-        calib = torch.cat([calib, torch.tensor([[0, 0, 0, 1]])])
-    else:
-        calib = calib[:, :-1]
-
-    return calib
-
-
-def project_depth(depth, calib, device="cuda:0"):
-    height, width = depth.shape[-2], depth.shape[-1]
-    
-    tensor_u = torch.arange(0, width, 1).repeat((height, 1)).to(device)
-    tensor_v = torch.arange(0, height, 1).repeat((width, 1)).permute(1, 0).to(device)
-
-    h_u = tensor_u*depth
-    h_v = tensor_v*depth
-
-    homogenous = torch.stack([h_u, h_v, depth], dim=-1)
-    
-    flattened_h = homogenous.view((height*width, 3)).permute(1, 0)
-    calibi = torch.inverse(calib)
-    projected = torch.matmul(calibi, flattened_h).permute(1, 0)
-    projected = projected.view((height, width, 3)).permute(2, 0, 1)
-    
-    return projected
-
-
-def convert_to_KITTI_format(rotations_array, translations_array):
-    raise NotImplementedError()
-
-
-def euler2matrix(r : torch.Tensor, convention="yxz", device="cuda") -> torch.Tensor:
-    c1 = torch.cos(r[0])
-    c2 = torch.cos(r[1])
-    c3 = torch.cos(r[2])
-
-    s1 = torch.sin(r[0])
-    s2 = torch.sin(r[1])
-    s3 = torch.sin(r[2])
-    
-    R = None
-
-    if convention == "yxz":
-        R = torch.tensor([  [c1*c3 + s1*s2*s3,  c3*s1*s2 - c1*s3,   c2*s1],
-                            [c2*s3,             c2*c3,              -s2],
-                            [c1*s2*s3 - c3*s1,  c1*c3*s2+s1*s3,     c1*c2]], device=device)
-    elif convention == "xyx":
-        R = torch.tensor([  [c2,                s2*s3,          c3*s2],
-                            [s1*s2,             c1*c3-c2*s1*s3, -c3*s3-c2*c3*s1],
-                            [-c1*s2,            c3*s1+c1*c2*s3, c1*c2*c3-s1*s3]], device=device)
-    elif convention == "yxy":
-        R = torch.tensor([  [c1*c3-c2*s1*s3,    s2*s1,          c2*s1*s1+c1*s3],
-                            [s2*s3,             c2,             -s2*c3],
-                            [-c3*s1-c2*c1*s3,   s2*c1,          c2*c1*c3-s1*s3]], device=device)
-
-    if R is None:
-        raise ArgumentError(None, "convention" + str(convention) + " is not supported")
-
-    return R
-
-
-def matrix2euler(R, convention="yxz", device="cpu"):
-    alpha, beta, gamma = None, None, None
-
-    if convention == "yxz":
-        alpha = torch.atan2(R[0, 2], R[2, 2])
-        beta = torch.atan2(-R[1, 2], torch.sqrt(1-R[1, 2]**2))
-        gamma = torch.atan2(R[1, 0], R[1, 1])
-    elif convention == "yxy":
-        alpha = torch.atan2(R[0, 1], R[2, 1])
-        beta = torch.atan2(torch.sqrt(1-R[1, 1]**2), R[1, 1])
-        gamma = torch.atan2(R[1, 0], -R[1, 2])
-
-    euler = torch.tensor([alpha, beta, gamma], device=device)
-    return euler
-
-
-def transform(rot : torch.Tensor, tr, device="cpu"):
-    # Euler to matrix rotation representation
-    rot = euler2matrix(rot, device=device)
-
-    # Concatenating rotation matrix and translation vector
-    mat = torch.cat([rot, tr.unsqueeze(1).to(device)], dim=1)
-
-    # Adding the extra row for homogenous matrix
-    mat = torch.cat([mat, torch.tensor([[0, 0, 0, 1]], device=device)], dim=0)
-
-    return mat
-
-
-# TODO Implement Homogenous Matrix to Euler+Trnaslation conversion
-
-
-def rel2abs(rotations, translations):
-    homogenous = []
-
-    instance_num = len(rotations)
-    for i in range(instance_num):
-        homogenous.append(transform(rotations[i], translations[i]))
-
-    global_scale = []
-    global_scale.append(homogenous[0])
-    for i in range(1, instance_num):
-        global_scale.append(torch.matmul(global_scale[i-1], homogenous[i]))
-        
-    global_scale = torch.stack(global_scale, dim=0)
-    global_pos = global_scale[:, :3, -1]
-    
-    return global_pos
 
---
+++ utils/transforms.py
@@ -0,0 +1,94 @@
+import torch
+
+
+def line2matrix(pose):
+    shape = matrix.shape
+    if len(shape) == 2:
+        matrix = torch.cat([matrix.view((3, 4)), torch.tensor([[0, 0, 0, 1]])], dim=0)
+    elif len(shape) == 3:
+        seq, _, __ = shape
+        matrix = torch.cat([matrix.view((seq, 3, 4)), torch.tensor([[0, 0, 0, 1]]).repeat((seq, 1, 1))], dim=0)
+    else:
+        raise Exception("Invalid shape for line to matrix transformation")
+
+    return matrix
+
+
+def euler2matrix(r : torch.Tensor, convention="yxz", device="cuda") -> torch.Tensor:
+    c1 = torch.cos(r[0])
+    c2 = torch.cos(r[1])
+    c3 = torch.cos(r[2])
+
+    s1 = torch.sin(r[0])
+    s2 = torch.sin(r[1])
+    s3 = torch.sin(r[2])
+    
+    R = None
+
+    if convention == "yxz":
+        R = torch.tensor([  [c1*c3 + s1*s2*s3,  c3*s1*s2 - c1*s3,   c2*s1],
+                            [c2*s3,             c2*c3,              -s2],
+                            [c1*s2*s3 - c3*s1,  c1*c3*s2+s1*s3,     c1*c2]], device=device)
+    elif convention == "xyx":
+        R = torch.tensor([  [c2,                s2*s3,          c3*s2],
+                            [s1*s2,             c1*c3-c2*s1*s3, -c3*s3-c2*c3*s1],
+                            [-c1*s2,            c3*s1+c1*c2*s3, c1*c2*c3-s1*s3]], device=device)
+    elif convention == "yxy":
+        R = torch.tensor([  [c1*c3-c2*s1*s3,    s2*s1,          c2*s1*s1+c1*s3],
+                            [s2*s3,             c2,             -s2*c3],
+                            [-c3*s1-c2*c1*s3,   s2*c1,          c2*c1*c3-s1*s3]], device=device)
+
+    if R is None:
+        raise Exception("Convention " + str(convention) + " is not supported")
+
+    return R
+
+
+def matrix2euler(R, convention="yxz", device="cpu"):
+    alpha, beta, gamma = None, None, None
+
+    if convention == "yxz":
+        alpha = torch.atan2(R[0, 2], R[2, 2])
+        beta = torch.atan2(-R[1, 2], torch.sqrt(1-R[1, 2]**2))
+        gamma = torch.atan2(R[1, 0], R[1, 1])
+    elif convention == "yxy":
+        alpha = torch.atan2(R[0, 1], R[2, 1])
+        beta = torch.atan2(torch.sqrt(1-R[1, 1]**2), R[1, 1])
+        gamma = torch.atan2(R[1, 0], -R[1, 2])
+
+    euler = torch.tensor([alpha, beta, gamma], device=device)
+    return euler
+
+
+def transform(rot : torch.Tensor, tr, device="cpu"):
+    # Euler to matrix rotation representation
+    rot = euler2matrix(rot, device=device)
+
+    # Concatenating rotation matrix and translation vector
+    mat = torch.cat([rot, tr.unsqueeze(1).to(device)], dim=1)
+
+    # Adding the extra row for homogenous matrix
+    mat = torch.cat([mat, torch.tensor([[0, 0, 0, 1]], device=device)], dim=0)
+
+    return mat
+
+
+# TODO Implement Homogenous Matrix to Euler+Translation conversion
+
+
+def rel2abs(rotations, translations):
+    homogenous = []
+
+    instance_num = len(rotations)
+    for i in range(instance_num):
+        homogenous.append(transform(rotations[i], translations[i]))
+
+    global_scale = [torch.eye(4, dtype=homogenous[0].dtype)]
+    for i in range(0, instance_num):
+        global_scale.append(torch.matmul(global_scale[i], homogenous[i]))
+        
+    global_scale = torch.stack(global_scale, dim=0)
+    #global_pos = global_scale[:, :3, -1]
+    
+    return global_scale
+
