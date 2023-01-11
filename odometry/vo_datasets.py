9641d9b8de7cf614115662de019d0c65f91a0ced
bogarrichard <bgyricsi@gmail.com>
2023-01-11 15:15:30 +0100

    Odometry dataset & loss dev

--- odometry/vo_datasets.py
+++ odometry/vo_datasets.py
@@ -7,7 +7,8 @@ from GMA.core.utils.utils import InputPadder
 from torchvision.transforms import Resize
 import torchvision.io as io
 
-from utils.transforms import matrix2euler, line2matrix
+from utils.transforms import matrix2euler, line2matrix, abs2rel
+from tqdm import trange
 
 
 class OdometryDataset(data.Dataset):
@@ -18,6 +19,7 @@ class OdometryDataset(data.Dataset):
     def preprocess_poses_euler(self,  pose1, pose2):
         # Stacking the matrix rows stored in the lines of the array
         pose1 = line2matrix(pose1)
+
         inverted1 = torch.inverse(pose1)
 
         pose2 = line2matrix(pose2)
@@ -215,6 +217,200 @@ class FlowKittiDataset(OdometryDataset):
             flows = self.resize(flows)
             if reverse:
                 flows = -1.0*flows
+                if self.N > 1:
+                    flows = torch.flip(flows, dims=[0])
+                
+            return flows, delta_rotations, delta_translations
+
+
+class FlowKittiDataset2(OdometryDataset):
+    def __init__(
+        self, 
+        data_path : str, 
+        sequences : list = ['00'], 
+        augment = False, 
+        sequence_length : int = 4, 
+        device : str ='cuda'
+    ) -> None:
+        super(FlowKittiDataset2, self).__init__()
+
+        self.sequences = sequences
+        if type(augment) is bool:
+            self.augment = 0 if augment else 1
+        else:
+            self.augment = augment
+        self.N = sequence_length
+        self.device = device
+
+        self.data_path = path.join(data_path, 'dataset')
+        self.sequence_lengths = [0]
+        self.sequence_poses = []
+
+        # Checking type of sequences
+        assert type(sequences) is list, "Sequences should be given as a list of strings!"
+        for sequence in sequences:
+            # Checking type and existence of each sequence
+            assert type(sequence) is str, "Sequence should be given as a string!"
+            if not path.isfile(path.join(self.data_path, "poses", sequence+".txt")):
+                raise Exception("Sequence " + sequence + " doesn't exist!")
+            
+            im_path = path.join(self.data_path, "sequences", sequence, "image_2")
+            sequence_length = len(glob.glob(im_path+"/*.png"))-self.N
+            self.sequence_lengths.append(self.sequence_lengths[-1]+sequence_length) 
+            
+            pose = np.loadtxt(path.join(self.data_path, 'poses', sequence+'.txt'), dtype=np.double)
+            self.sequence_poses.append(torch.from_numpy(pose))
+
+        self.sequence_lengths = self.sequence_lengths[1:]
+        assert len(sequences) == len(self.sequence_lengths), "Sequence lengths are not as many as sequences"
+
+        img = io.read_image(path.join(self.data_path, "sequences", '00', "image_2", "000000.png")).float()
+        self.resize = Resize((img.shape[-2], img.shape[-1]))
+        self.padder = InputPadder(img.shape)
+
+
+    def __len__(self):
+        return self.sequence_lengths[-1]
+
+
+    def __getitem__(self, index):
+            reverse = (self.augment + torch.rand(1)) < 0.5
+            #log("Reverse flow augmentation: ", augment)
+
+            sequence_index = 0
+            index_offset = 0
+            # Finding the sequence
+            for i in range(len(self.sequence_lengths)):
+                if index >= self.sequence_lengths[i]:
+                    sequence_index = sequence_index+1
+                    index_offset = self.sequence_lengths[i]
+
+            index = index-index_offset
+
+            # Getting pose difference as rotation and translation vectors
+            poses_n = [self.sequence_poses[sequence_index][index+i, :] for i in range(0, self.N+1)]
+            
+            if reverse:
+                poses_n.reverse()
+            delta_transforms = [abs2rel(poses_n[i], poses_n[i+1]) for i in range(0, (self.N))]
+
+            delta_rotations = torch.stack([delta_transforms[i][0] for i in range(len(delta_transforms))])
+            delta_translations = torch.stack([delta_transforms[i][1] for i in range(len(delta_transforms))])
+
+            flow_path = path.join(self.data_path, "flows2", self.sequences[sequence_index])
+
+            # Generating flow file names from index
+            flow_files = ['0'*(6-len(str(index+i))) + str(index+i) + ".pt" for i in range(0, self.N)]
+            flows = [torch.load(path.join(flow_path, flow_file)) for flow_file in flow_files]
+            flows = torch.cat(flows, dim=0)
+            #flows = self.resize(flows)
+
+            if flows.size()[-1] > 1232:
+                diff = flows.size()[-1]-1232
+                flows = flows[:, :, :, diff//2:-diff//2].squeeze()
+
+            if reverse:
+                flows = -1.0*flows
+                if self.N > 1:
+                    flows = torch.flip(flows, dims=[0])
+
+            return flows, delta_rotations, delta_translations
+
+
+class FlowKittiDataset3(OdometryDataset):
+    def __init__(
+        self, 
+        data_path : str, 
+        sequences : list = ['00'], 
+        augment = False, 
+        sequence_length : int = 4, 
+        device : str ='cuda'
+    ) -> None:
+        super(FlowKittiDataset3, self).__init__()
+
+        self.sequences = sequences
+        if type(augment) is bool:
+            self.augment = 0 if augment else 1
+        else:
+            self.augment = augment
+        self.N = sequence_length
+        self.device = device
+
+        self.data_path = path.join(data_path, 'dataset')
+        self.sequence_lengths = [0]
+        self.sequence_poses = []
+
+        # Checking type of sequences
+        assert type(sequences) is list, "Sequences should be given as a list of strings!"
+        for sequence in sequences:
+            # Checking type and existence of each sequence
+            assert type(sequence) is str, "Sequence should be given as a string!"
+            if not path.isfile(path.join(self.data_path, "poses", sequence+".txt")):
+                raise Exception("Sequence " + sequence + " doesn't exist!")
+            
+            im_path = path.join(self.data_path, "sequences", sequence, "image_2")
+            sequence_length = len(glob.glob(im_path+"/*.png"))-self.N
+            self.sequence_lengths.append(self.sequence_lengths[-1]+sequence_length) 
+            
+            pose = np.loadtxt(path.join(self.data_path, 'poses', sequence+'.txt'), dtype=np.double)
+            self.sequence_poses.append(torch.from_numpy(pose))
+
+        self.sequence_lengths = self.sequence_lengths[1:]
+        assert len(sequences) == len(self.sequence_lengths), "Sequence lengths are not as many as sequences"
+
+        img = io.read_image(path.join(self.data_path, "sequences", '00', "image_2", "000000.png")).float()
+        self.resize = Resize((img.shape[-2], img.shape[-1]))
+        self.padder = InputPadder(img.shape)
+
+        self.flows_all = []
+        for sequence in sequences:
+            seq_path = path.join(self.data_path, "flows2", sequence, "*.pt")
+            files = sorted(glob.glob(seq_path))
+            self.flows_all.append([])
+
+            for i in trange(len(files)):
+                A = torch.load(files[i], map_location='cpu')
+
+                if A.size()[-1] > 1232:
+                    diff = A.size()[-1]-1232
+                    A = A[:, :, :, diff//2:-diff//2]
+
+                self.flows_all[-1].append(A)
+            self.flows_all[-1] = torch.cat(self.flows_all[-1], dim=0)
+
+
+    def __len__(self):
+        return self.sequence_lengths[-1]
+
+
+    def __getitem__(self, index):
+            reverse = (self.augment + torch.rand(1)) < 0.5
+            #log("Reverse flow augmentation: ", augment)
+
+            sequence_index = 0
+            index_offset = 0
+            # Finding the sequence
+            for i in range(len(self.sequence_lengths)):
+                if index >= self.sequence_lengths[i]:
+                    sequence_index = sequence_index+1
+                    index_offset = self.sequence_lengths[i]
+
+            index = index-index_offset
+
+            # Getting pose difference as rotation and translation vectors
+            poses_n = [self.sequence_poses[sequence_index][index+i, :] for i in range(0, self.N+1)]
+
+            if reverse:
+                poses_n.reverse()
+            delta_transforms = [abs2rel(poses_n[i], poses_n[i+1]) for i in range(0, (self.N))]
+
+            delta_rotations = torch.stack([delta_transforms[i][0] for i in range(len(delta_transforms))])
+            delta_translations = torch.stack([delta_transforms[i][1] for i in range(len(delta_transforms))])
+
+            flows = self.flows_all[sequence_index][index:index+self.N]
+
+            if reverse:
+                flows = (-1)*flows
                 if self.N > 1:
                     flows = torch.flip(flows, dims=[0])
                 
--- odometry/vo_loss.py
+++ odometry/vo_loss.py
@@ -1,11 +1,17 @@
 import torch
+from torch import nn
 from utils.helpers import  log
 from utils.transforms import matrix2euler, transform
 
 
 class CLVO_Loss():
 
-    def __init__(self, alpha=1, w=3, device="cuda:0"):
+    def __init__(
+        self, 
+        alpha=1, 
+        w=3, 
+        device="cuda:0"
+    ):
         self.rot_weight = (1.0/torch.tensor([0.0175, 0.0031, 0.0027], device=device)).unsqueeze(0)
         self.tr_weight  = (1.0/torch.tensor([0.0219, 0.0260, 1.0917], device=device)).unsqueeze(0)
         
@@ -16,30 +22,31 @@ class CLVO_Loss():
         self.w = w
 
 
-    def __call__(self, pred_transforms, true_transforms, device='cuda:0'):
+    def __call__(
+        self, 
+        pred_rot, 
+        pred_tr, 
+        true_rot, 
+        true_tr, 
+        device='cuda:0'
+    ):
 
-        # --------------------------------------
-        # Dimension checking and data extraction
-        # --------------------------------------
+
         w = self.w
-        assert len(pred_transforms) == len(true_transforms), "Loss: Length of pred and true are not equal"
-
-        pred_rot, pred_tr = pred_transforms
-        true_rot, true_tr = true_transforms
 
         # -------------------
         # Relative pose loss
         # -------------------
-        L_rel = 0
-        L_rel = self.transform_loss1(pred_rot, pred_tr, true_rot, true_tr).sum(-1)
+        L_rel = self.transform_loss(pred_rot, pred_tr, true_rot, true_tr).sum(-1)
 
         # -------------------
         # Composite pose loss
         # -------------------
-        com_loss = []
-        for i in range(len(pred_rot)):
-            com_loss.append(self.com_loss([pred_rot[i], pred_tr[i]], [true_rot[i], true_tr[i]], w=w, device=device))
-        com_loss = torch.stack(com_loss, dim=0).sum(-1)
+        com_loss = 0
+        #com_loss = []
+        #for i in range(len(pred_rot)):
+        #    com_loss.append(self.com_loss(pred_rot[i], pred_tr[i], true_rot[i], true_tr[i], w=w, device=device))
+        #com_loss = torch.stack(com_loss, dim=0).sum(-1)
         
         # ---------------
         # Total pose loss
@@ -48,11 +55,15 @@ class CLVO_Loss():
         return L_total.mean()
 
 
-    def com_loss(self, pred_transforms, true_transforms, w, device='cuda'):
-
-        # Separating rotations and translations
-        pred_rot, pred_tr = pred_transforms
-        true_rot, true_tr = true_transforms
+    def com_loss(
+        self, 
+        pred_rot, 
+        pred_tr, 
+        true_rot, 
+        true_tr, 
+        w, 
+        device='cuda'
+    ):
 
         # Converting euler vector and translation vector to homogenous transformation matrix
         pred_homogenous_array = []
@@ -78,14 +89,20 @@ class CLVO_Loss():
             true_comm_rot = matrix2euler(true_comm[:3, :3], device=device)
             true_comm_tr = true_comm[:3, -1]
             
-            loss = self.transform_loss1(pred_comm_rot, pred_comm_tr, true_comm_rot, true_comm_tr)
+            loss = self.transform_loss(pred_comm_rot, pred_comm_tr, true_comm_rot, true_comm_tr)
             losses.append(loss)
 
         loss = torch.stack(losses, dim=0)
         return loss
 
 
-    def transform_loss1(self, pred_rotation, pred_translation, true_rotation, true_translation):
+    def transform_loss(
+        self, 
+        pred_rotation, 
+        pred_translation, 
+        true_rotation, 
+        true_translation
+    ):
 
         diff_rotation = (pred_rotation-true_rotation)
         diff_translation = (pred_translation-true_translation)
@@ -94,20 +111,5 @@ class CLVO_Loss():
         norm_translation = (diff_translation**2).sum(dim=-1)
 
         loss = self.delta*norm_translation + self.khi*norm_rotation
-        return loss
-
-
-    def transform_loss2(self, pred_rotation, pred_translation, true_rotation, true_translation):
-
-        diff_rotation = (pred_rotation-true_rotation)
-        diff_translation = (pred_translation-true_translation)
-
-        diff_rotation = (diff_rotation*self.rot_weight).squeeze()
-        diff_translation = (diff_translation*self.tr_weight).squeeze()
-        
-        norm_rotation = torch.linalg.vector_norm(diff_rotation, dim=-1, ord=2)
-        norm_translation = torch.linalg.vector_norm(diff_translation, dim=-1, ord=2)
-
-        loss = norm_translation + norm_rotation
         return loss
 
