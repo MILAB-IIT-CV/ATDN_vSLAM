8539173bf422633f4cf3c0c5a9b96f299e17b071
bogarrichard <bgyricsi@gmail.com>
2023-02-25 13:04:50 +0100

    Adding experimental modules

---
+++ atdn_vslam/depth/network.py
@@ -0,0 +1,86 @@
+from torchvision.transforms.functional import crop
+from torchvision.transforms import Resize
+from torch import nn
+
+from ..layers import conv
+from ..utils.helpers import log
+
+    
+class DepthPredictor(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        #self.net = nn.Sequential(
+        #    conv.Conv(in_channels=8, out_channels=16, kernel_size=7, padding=3),
+        #    conv.Conv(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+        #    conv.Conv(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+        #    conv.Conv(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+        #    conv.Conv(in_channels=64, out_channels=32, kernel_size=3, padding=1),
+        #    conv.Conv(in_channels=32, out_channels=16, kernel_size=3, padding=1),
+        #    conv.Conv(in_channels=16, out_channels=8, kernel_size=3, padding=1),
+        #    nn.Conv2d(in_channels=8, out_channels=2, kernel_size=3, padding=1)
+        #)
+
+        self.stem = conv.Conv(in_channels=8, out_channels=8, kernel_size=7, stride=2, padding=3)
+
+        self.E1 = conv.ResidualConv(in_channels=8, out_channels=16, stride=2)
+        self.E2 = conv.ResidualConv(in_channels=16, out_channels=32, stride=2)
+        self.E3 = conv.ResidualConv(in_channels=32, out_channels=64, stride=2)
+        self.E4 = conv.ResidualConv(in_channels=64, out_channels=128, stride=2)
+        self.E5 = conv.ResidualConv(in_channels=128, out_channels=128, stride=2)
+        
+        self.bottleneck = nn.Sequential(
+            conv.ResidualConv(in_channels=128, out_channels=256),
+            conv.ResidualConv(in_channels=256, out_channels=256),
+            conv.ResidualConv(in_channels=256, out_channels=128),
+        )
+
+        self.De1 = conv.ConnectedDUC(in_channels=128, out_channels=128)
+        self.De2 = conv.ConnectedDUC(in_channels=128, out_channels=64)
+        self.De3 = conv.ConnectedDUC(in_channels=64, out_channels=32)
+        self.De4 = conv.ConnectedDUC(in_channels=32, out_channels=16)
+        self.De5 = conv.ConnectedDUC(in_channels=16, out_channels=2)
+
+        self.resize = Resize((376, 1241))
+
+
+    def forward(self, input):
+        #log(input.shape)
+        pre = self.stem(input)
+        #log(pre.shape)
+        down1 = self.E1(pre)
+        #log(down1.shape)
+        down2 = self.E2(down1)
+        #log(down2.shape)
+        down3 = self.E3(down2)
+        #log(down3.shape)
+        down4 = self.E4(down3)
+        #log(down4.shape)
+        down5 = self.E5(down4)
+        #log(down5.shape)
+
+        bottom = self.bottleneck(down5)
+
+        shape = bottom.shape
+        up5 = self.De1(bottom, crop(down5, 0, 0, shape[2], shape[3]))
+        #log(up5.shape)
+        shape = up5.shape
+        up4 = self.De2(up5, crop(down4, 0, 0, shape[2], shape[3]))
+        #log(up4.shape)
+        shape = up4.shape
+        up3 = self.De3(up4, crop(down3, 0, 0, shape[2], shape[3]))
+        #log(up3.shape)
+        shape = up3.shape
+        up2 = self.De4(up3, crop(down2, 0, 0, shape[2], shape[3]))
+        #log(up2.shape)
+        shape = up2.shape
+        up1 = self.De5(up2, crop(down1, 0, 0, shape[2], shape[3]))
+        #log(up1.shape)
+
+        depths = self.resize(up1)
+
+        return depths[:, 0, :, :], depths[:, 1, :, :]
+
+
+        #depths = self.net(input)
+        #return depths[:, 0, :, :], depths[:, 1, :, :]
---
+++ train_depth.py
@@ -0,0 +1,82 @@
+from datetime import datetime
+import math
+import torch
+from torch.utils.tensorboard.writer import SummaryWriter
+import torch.utils.tensorboard
+
+from torch.utils.data import DataLoader
+from atdn_vslam.utils.arguments import Arguments
+from atdn_vslam.utils.helpers import log
+from atdn_vslam.depth.network import DepthPredictor
+from atdn_vslam.depth.dataset import OdometryDepthDataset
+from atdn_vslam.depth.loss import Movement_Depth_Loss
+
+
+def train(model, loss_fn, dataloader, optimizer, scheduler, writer, epoch):
+    for batch, (imgs, flows, transforms, calib, masks) in enumerate(dataloader):
+        optimizer.zero_grad()
+        
+        imgs = imgs.to("cuda:0")
+        flows = flows.to("cuda:0")
+        transforms = transforms.to("cuda:0")
+        calib = calib.to("cuda:0")
+
+        input = torch.cat([flows, imgs[:, 0, :, :, :], imgs[:, 1, :, :, :]], dim=1)
+        
+        depth1, depth2 = model(input)
+
+        loss = loss_fn(depth1, depth2, flows, calib, transforms, masks)
+        
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+        writer.add_scalar('Loss', loss.item(), batch+epoch*len(dataloader))
+        writer.add_scalar('Learning Rate', scheduler.get_last_lr()[0], batch+epoch*len(dataloader))
+        
+        print("", end='\r')
+        print("Iteration: %d / %d \t|\t Loss: %5f" % (batch, len(dataloader), loss.item()), '\t|\t Learning rate: ', scheduler.get_last_lr(),  end='\n')
+
+
+def main():
+    args = Arguments.get_arguments()
+
+    dataset = OdometryDepthDataset(args.data_path, sequences=['00'], sequence_length=1)
+    dataloader = DataLoader(dataset, batch_size=8, shuffle=True, pin_memory=True, num_workers=2)
+
+    model = DepthPredictor().eval()
+    trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    log("Trainable parameters:", trainable_params)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+    scheduler_limit = math.floor(args.epochs*(len(dataset)//2-args.batch_size)/args.batch_size)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, 
+                                                     scheduler_limit, 
+                                                     eta_min=1e-6)
+
+    loss_fn = Movement_Depth_Loss()
+    now = datetime.now()
+    writer = SummaryWriter("loss_log/depth/tensorboard/"+str(now)[:10]+str(now.hour)+str(now.minute))
+    
+    imgs, flow, transforms, calib, masks = dataset[5]
+    im1, im2 = imgs[0].unsqueeze(0), imgs[1].unsqueeze(0)
+    flow = flow.unsqueeze(0)
+    input = torch.cat([flow, im1, im2], dim=1).to("cpu")
+    with torch.no_grad():
+        writer.add_graph(model, input)
+    
+
+    model = model.to("cuda:0").train()
+    epochs = 20
+    print("============================================ ", "Training", " =============================================\n")
+    for epoch in range(epochs):
+        print("------------------------------------------ ", "Epoch ", epoch+1, "/", epochs, " ------------------------------------------\n")
+        
+        train(model, loss_fn, dataloader, optimizer, scheduler, writer, epoch)
+        log("Saving model to checkpoints/depth_predictor.pth")
+        torch.save(model.state_dict(), "checkpoints/depth_predictor.pth")
+
+    writer.flush()
+    writer.close()
+
+main()
